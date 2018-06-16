975e822337af4b0fb6ad38f31c52bf7eff7e3c17
Jakub Miazek <the@grillazz.com>
2018-06-16 14:17:09 +0200

    models and migrations

--- yetAnotherDudeApp/issueTracker/models.py
+++ yetAnotherDudeApp/issueTracker/models.py
@@ -1,3 +1,43 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Status(models.Model):
+    code = models.CharField(max_length=20)
+
+    class Meta:
+        verbose_name = 'status'
+        verbose_name_plural = 'statuses'
+
+    def __unicode__(self):
+        return self.code
+
+
+class Priority(models.Model):
+    code = models.CharField(max_length=20)
+
+    class Meta:
+        verbose_name = 'priority'
+        verbose_name_plural = 'priorities'
+
+    def __unicode__(self):
+        return self.code
+
+
+class Issue(models.Model):
+    title = models.CharField(max_length=100)
+    submitted_date = models.DateField(auto_now_add=True)
+    modified_date = models.DateField(auto_now=True)
+    submitter = models.ForeignKey(User, verbose_name='submitter', related_name="submitter", on_delete=False)
+    assigned_to = models.ForeignKey(User, verbose_name='assigned to', on_delete=False)
+    description = models.TextField(blank=True)
+    status = models.ForeignKey(Status, verbose_name='status', related_name="status", on_delete=False)
+    priority = models.ForeignKey(Priority, verbose_name='priority', related_name="priority", on_delete=False)
+
+    class Meta:
+        verbose_name = 'issue'
+        verbose_name_plural = 'issues'
+        ordering = ('status', 'priority', 'submitted_date', 'title')
+
+    def __unicode__(self):
+        return self.title
--- yetAnotherDudeApp/yetAnotherDudeApp/settings.py
+++ yetAnotherDudeApp/yetAnotherDudeApp/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['0.0.0.0']
 # Application definition
 
 INSTALLED_APPS = [
+    'issueTracker.apps.IssuetrackerConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
