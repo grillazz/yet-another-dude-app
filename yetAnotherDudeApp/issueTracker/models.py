6c6c9ade8ef1badc0ae79c5951a883a4988e3926
Jakub Miazek <the@grillazz.com>
2018-06-16 14:47:43 +0200

    models changes

--- yetAnotherDudeApp/issueTracker/models.py
+++ yetAnotherDudeApp/issueTracker/models.py
@@ -9,7 +9,7 @@ class Status(models.Model):
         verbose_name = 'status'
         verbose_name_plural = 'statuses'
 
-    def __unicode__(self):
+    def __str__(self):
         return self.code
 
 
@@ -20,7 +20,7 @@ class Priority(models.Model):
         verbose_name = 'priority'
         verbose_name_plural = 'priorities'
 
-    def __unicode__(self):
+    def __str__(self):
         return self.code
 
 
@@ -28,16 +28,13 @@ class Issue(models.Model):
     title = models.CharField(max_length=100)
     submitted_date = models.DateField(auto_now_add=True)
     modified_date = models.DateField(auto_now=True)
-    submitter = models.ForeignKey(User, verbose_name='submitter', related_name="submitter", on_delete=False)
-    assigned_to = models.ForeignKey(User, verbose_name='assigned to', on_delete=False)
+    submitter = models.ForeignKey(User, verbose_name='submitter', related_name="submitter", on_delete=models.CASCADE)
+    assigned_to = models.ForeignKey(User, verbose_name='assigned to', on_delete=models.CASCADE)
     description = models.TextField(blank=True)
-    status = models.ForeignKey(Status, verbose_name='status', related_name="status", on_delete=False)
-    priority = models.ForeignKey(Priority, verbose_name='priority', related_name="priority", on_delete=False)
+    status = models.ForeignKey(Status, verbose_name='status', related_name="status", on_delete=models.CASCADE)
+    priority = models.ForeignKey(Priority, verbose_name='priority', related_name="priority", on_delete=models.CASCADE)
 
     class Meta:
         verbose_name = 'issue'
         verbose_name_plural = 'issues'
         ordering = ('status', 'priority', 'submitted_date', 'title')
-
-    def __unicode__(self):
-        return self.title
